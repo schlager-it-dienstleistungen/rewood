783974ecea9bb4619a050eb4f1554f5a8b39c524
ZeRoberto27 <robert.schlager@gmail.com>
2019-09-19 18:25:15 +0200

    initial demo 9 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,54 @@
+// Angular
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+// Components
+import { BaseComponent } from './views/theme/base/base.component';
+import { ErrorPageComponent } from './views/theme/content/error-page/error-page.component';
+// Auth
+import { AuthGuard } from './core/auth';
 
+const routes: Routes = [
+	{
+		path: 'auth',
+		loadChildren: './views/pages/auth/auth.module#AuthModule'
+	},
+	{
+		path: '',
+		component: BaseComponent,
+		canActivate: [AuthGuard],
+		children: [
+			{
+				path: 'dashboard',
+				loadChildren: './views/pages/dashboard/dashboard.module#DashboardModule'
+			},
+			{
+				path: 'builder',
+				loadChildren: './views/theme/content/builder/builder.module#BuilderModule'
+			},
+			{
+				path: 'error/403',
+				component: ErrorPageComponent,
+				data: {
+					type: 'error-v6',
+					code: 403,
+					title: '403... Access forbidden',
+					desc: 'Looks like you don\'t have permission to access for requested page.<br> Please, contact administrator'
+				}
+			},
+			{path: 'error/:type', component: ErrorPageComponent},
+			{path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+			{path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+		]
+	},
 
-const routes: Routes = [];
+	{path: '**', redirectTo: 'error/403', pathMatch: 'full'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes)
+	],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/views/pages/pages.module.ts
@@ -0,0 +1,20 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// Partials
+import { PartialsModule } from '../partials/partials.module';
+// Pages
+import { CoreModule } from '../../core/core.module';
+
+@NgModule({
+	declarations: [],
+	exports: [],
+	imports: [
+		CommonModule,
+		CoreModule,
+		PartialsModule
+	],
+	providers: []
+})
+export class PagesModule {
+}
