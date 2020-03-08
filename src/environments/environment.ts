98dec9170b28643298c869ad05326e3969c82509
ZeRoberto27 <robert.schlager@gmail.com>
2020-03-08 19:23:20 +0100

    rename project and repository to rewood

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,14 +7,14 @@ export const environment = {
 	isMockEnabled: true, // You have to switch this, when your real back-end is done
 	authTokenKey: 'authce9d77b308c149d5992a80073637e4d5',
 	firebase: {
-		apiKey: 'AIzaSyCpveFN-QVgBM0LPQYiXIjtybkr8FS9o4Q',
-		authDomain: 'secondwood-82300.firebaseapp.com',
-		databaseURL: 'https://secondwood-82300.firebaseio.com',
-		projectId: 'secondwood-82300',
-		storageBucket: 'secondwood-82300.appspot.com',
-		messagingSenderId: '699444831782',
-		appId: '1:699444831782:web:1ef3bf28023da1168528e7',
-		measurementId: 'G-9HGLVVNBEX'
+		apiKey: 'AIzaSyAqmWi9KjWwRaGYjmRiDUp5UReGuAyt3Ro',
+		authDomain: 'rewood-a7ef8.firebaseapp.com',
+		databaseURL: 'https://rewood-a7ef8.firebaseio.com',
+		projectId: 'rewood-a7ef8',
+		storageBucket: 'rewood-a7ef8.appspot.com',
+		messagingSenderId: '1043942842507',
+		appId: '1:1043942842507:web:e66687677c51097f8ff48e',
+		measurementId: 'G-HB50Q1Z005'
 	}
 };
 
