44ffc1986772aa0198fd1c97a08860272f35e9bf
ZeRoberto27 <robert.schlager@gmail.com>
2020-04-22 09:37:34 +0200

    NewProduct: Implement Dummy Wizard

--- src/app/views/pages/admin/create-product/create-product.component.ts
+++ src/app/views/pages/admin/create-product/create-product.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'rw-create-product',
-  templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.scss']
+	selector: 'rw-create-product',
+	templateUrl: './create-product.component.html',
+	styleUrls: ['./create-product.component.scss']
 })
-export class CreateProductComponent implements OnInit {
+export class CreateProductComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+	@ViewChild('wizard', {static: true}) el: ElementRef;
+	submitted = false;
+	model: any = {
+		fname: 'John',
+		lname: 'Wick',
+		phone: '+61412345678',
+		email: 'john.wick@reeves.com',
+		address1: 'Address Line 1',
+		address2: 'Address Line 2',
+		postcode: '3000',
+		city: 'Melbourne',
+		state: 'VIC',
+		country: 'AU',
+		delivery: 'overnight',
+		packaging: 'regular',
+		preferreddelivery: 'morning',
+		locaddress1: 'Address Line 1',
+		locaddress2: 'Address Line 2',
+		locpostcode: '3072',
+		loccity: 'Preston',
+		locstate: 'VIC',
+		loccountry: 'AU',
+		ccname: 'John Wick',
+		ccnumber: '4444 3333 2222 1111',
+		ccmonth: '01',
+		ccyear: '21',
+		cccvv: '123',
+	};
 
-  ngOnInit() {
-  }
+	constructor() { }
 
+	ngOnInit() {
+	}
+
+	ngAfterViewInit(): void {
+		// Initialize form wizard
+		const wizard = new KTWizard(this.el.nativeElement, {
+			startStep: 1
+		});
+
+		// Validation before going to next page
+		wizard.on('beforeNext', wizardObj => {
+			// https://angular.io/guide/forms
+			// https://angular.io/guide/form-validation
+
+			// validate the form and use below function to stop the wizard's step
+			// wizardObj.stop();
+
+			const validationOK = true;
+			if (!validationOK) {
+				wizardObj.stop();
+			}
+		});
+
+		// Change event
+		/*wizard.on('change', function (wizard) {
+			setTimeout(function () {
+				KTUtil.scrollTop();
+			}, 500);
+		});*/
+	}
+
+	onSubmit() {
+		this.submitted = true;
+	}
 }
