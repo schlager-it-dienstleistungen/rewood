89aad926d4db96c2d5f15ee2ba60609c5b79e48e
ZeRoberto27 <robert.schlager@gmail.com>
2020-04-22 09:33:11 +0200

    NewSupplier: Implement Dummy Wizard

--- src/app/views/pages/admin/admin.module.ts
+++ src/app/views/pages/admin/admin.module.ts
@@ -5,12 +5,19 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { CreateSupplierComponent } from './create-supplier/create-supplier.component';
 import { CreateProductComponent } from './create-product/create-product.component';
+import { FormsModule } from '@angular/forms';
+import { PortletModule } from '../../partials/content/general/portlet/portlet.module';
+import { PartialsModule } from '../../partials/partials.module';
+import { CoreModule } from 'src/app/core/core.module';
 
 
 @NgModule({
 	declarations: [AdminComponent, CreateSupplierComponent, CreateProductComponent],
 	imports: [
 		CommonModule,
+		FormsModule,
+		PartialsModule,
+		CoreModule,
 		AdminRoutingModule
 	]
 })
