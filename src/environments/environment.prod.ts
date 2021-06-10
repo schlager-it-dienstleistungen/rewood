84c902cd29392050d2a85c136cda11a7f961c46b
ZeRoberto27 <robert.schlager@gmail.com>
2021-06-10 09:36:37 +0200

    prepare Production

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-	production: true,
-	appVersion: 'v01_20210609_01',
-	isMockEnabled: false, // You have to switch this, when your real back-end is done
-	authTokenKey: 'x'
-};
