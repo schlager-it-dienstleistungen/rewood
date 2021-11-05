cbed9dfd393ccfb892c5d561a756f236707202ce
ZeRoberto27 <robert.schlager@gmail.com>
2021-11-05 08:22:20 +0100

    Translate to EN #97

--- src/app/core/_config/i18n/en.ts
+++ src/app/core/_config/i18n/en.ts
@@ -1,4 +1,4 @@
-// USA
+// English
 export const locale = {
 	lang: 'en',
 	data: {
@@ -8,6 +8,21 @@ export const locale = {
 		HOME: {
 			LATEST_PRODUCTS: 'Latest Products'
 		},
+		GENERAL: {
+			WELCOME_TO_REWOOD: 'Welcome to REWOOD',
+			CREDO: 'Rewood ist der virtuelle Marktplatz für Holz-Produkte in Top-Qualität von unterschiedlichen, namhaften Produzenten, welche sich von Standard-Holz-Erzeugnissen nur durch geringfügige Abweichungen unterscheiden.',
+			PHONE: 'Phone',
+			EMAIL: 'E-Mail',
+			ACTIONS: 'Actions',
+			COMPANY: 'Company',
+			ROLE: 'role',
+			ROLES: 'roles',
+			ADDRESS1: 'Address 1',
+			ADDRESS2: 'Address 2',
+			POSTCODE: 'ZIP',
+			CITY: 'City',
+			COUNTRY: 'Country'
+		},
 		MENU: {
 			NEW: 'new',
 			ACTIONS: 'Actions',
@@ -16,7 +31,119 @@ export const locale = {
 			FEATURES: 'Features',
 			APPS: 'Apps',
 			HOME: 'Home',
-			PRODUCTS: 'Products'
+			PRODUCTS: 'Products',
+			PRODUCTS_BYCATEGORIE: 'by category',
+			PRODUCTS_ALL: 'all products',
+			ADMIN: 'Admin',
+			ADMIN_SUPPLIERS: 'Supplier',
+			ADMIN_PRODUCTS: 'Products',
+			ADMIN_USERS: 'User',
+		},
+		FOOTER: {
+			ABOUT: 'About',
+			TEAM: 'Team',
+			CONTACT: 'Contact',
+			AGB: 'Conditions'
+		},
+		PRODUCT: {
+			SHOW_ALL_PRODUCTS: 'show all products',
+			NUMBER_OF_AVAILABLE: 'number of available products',
+			CATEGORY: 'Category',
+			SUBCATEGORY: 'Subcategory',
+			TITLE: 'Title',
+			LENGTH: 'Length (mm)',
+			WIDTH: 'Width (mm)',
+			HEIGHT: 'Height (in mm)',
+			VOLUME_PER_PIECE: 'Volume/Piece (in m³)',
+			QUANTITY: 'Quantity (in m³)',
+			TOTAL_VOLUME: 'Total Volume (in m³)',
+			PRICE_PER_CUBIC: 'Price per m³',
+			PRICE: 'Price',
+			DESCRIPTION: 'Description',
+			STATE: 'State',
+			ALL_PRODUCTS: 'All Products',
+			FILTER_PRODUCTS: 'Filter Products',
+			BY_SUPPLIER: 'Products by Supplier',
+			BY_CATEGORY: 'Products by Category',
+			NEW: 'New Product',
+			NEW_TOOLTIP: 'Create a new product',
+			EDIT_TOOLTIP: 'Edit product',
+			DEL_TOOLTIP: 'Delete product',
+			BOOK: 'book',
+			EDIT: {
+				PRODUCT_INFO: 'product information',
+				DESCRIPTION: 'Describe the product',
+				DIMENSIONS: 'Diomensions',
+				DIMENSIONS_DESC: 'Specify the dimensions',
+				LOCATION: 'Product location',
+				LOCATION_DESC: 'Product location',
+				PRODUCT_NR: 'Productnr',
+				PICTURES: 'Pictures'
+			}
+		},
+		SUPPLIER: {
+			ALL_SUPPLIERS: 'All suppliers',
+			FILTER_SUPPLIERS: 'Filter suppliers',
+			NUMBER: 'Number',
+			SUPPLIER: 'Supplier',
+			LOCATION: 'Location',
+			FSC: 'FSC',
+			PEFC: 'PEFC',
+			NEW: 'New supplier',
+			NEW_TOOLTIP: 'Create a new supplier',
+			EDIT_TOOLTIP: 'Edit supplier',
+			DEL_TOOLTIP: 'Delete suppplier',
+			EDIT: {
+				SUPPLIER_INFO: 'Supplier information',
+				LOCATION: 'Location',
+				LOCATION_DESC: 'Supplier address',
+				SUPPLIER_NR: 'Supplier Nr.',
+				HOMEPAGE: 'Homepage',
+				VAT: 'VAT'
+			}
+		},
+		USER: {
+			USER: 'User',
+			ALL_USER: 'All user',
+			FILTER_USER: 'Filter User',
+			USERNAME: 'Username',
+			FIRSTNAME: 'Firstname',
+			LASTNAME: 'Lastname',
+			NAME: 'Name',
+			NEW: 'New User',
+			NEW_TOOLTIP: 'Crate a new user',
+			EDIT_TOOLTIP: 'Edit user',
+			DEL_TOOLTIP: 'Delete user',
+			EDIT: {
+				USER_INFO: 'User information',
+				NOTIFICATION: 'Notification',
+				NOTIFICATION_DESC: 'For which product categories should be made a notification',
+				ROLES: 'Roles',
+				ROLES_DESC: 'In which roles does the user appear',
+				SELECT_ROLE: 'Select a role',
+				ASSIGNED_ROLES: 'Selected roles',
+				DEL_ROLE: 'Delete role',
+				SELECT_CATEGORY: 'Select category',
+				ASSIGNED_CATEGORIES: 'Selected Categories',
+				DEL_CATEGORY: 'Delete category'
+			}
+		},
+		NEW: {
+			GENERAL_INFO: 'Provide general information',
+			FINISH: 'Finish',
+			FINISH_DESC: 'Summary and conclusion',
+			NEXT_STEP: 'Next stepp',
+			BACK: 'Back',
+			ASSIGN: 'Assign',
+			SAVE: 'Save',
+			INVALID_FORM: 'Fehlerhafte Eingabe - Bitte füllen Sie alle Daten korrekt aus!',
+			INVALID_SUPPLIER_FOR_PRODUCT: 'No valid supplier for product!',
+			INVALID_PRODUCT_NUMBER: 'Product has no valid productnumber!',
+			INVALID_ROLE_SELECTED: 'Invalid Fehlerhafte Eingabe - Es muss eine Rolle ausgewählt werden!',
+			INVALID_SUPPLIER_FOR_USER: 'Fehlerhafte Eingabe - Es muss ein Lieferant gewählt werden!'
+		},
+		INDICES: {
+			FROM: 'Currencies from date'
 		},
 		AUTH: {
 			GENERAL: {
@@ -62,47 +189,8 @@ export const locale = {
 				MIN_LENGTH_FIELD: 'Minimum field length:',
 				MAX_LENGTH_FIELD: 'Maximum field length:',
 				INVALID_FIELD: 'Field is not valid',
-			}
-		},
-		ECOMMERCE: {
-			COMMON: {
-				SELECTED_RECORDS_COUNT: 'Selected records count: ',
-				ALL: 'All',
-				SUSPENDED: 'Suspended',
-				ACTIVE: 'Active',
-				FILTER: 'Filter',
-				BY_STATUS: 'by Status',
-				BY_TYPE: 'by Type',
-				BUSINESS: 'Business',
-				INDIVIDUAL: 'Individual',
-				SEARCH: 'Search',
-				IN_ALL_FIELDS: 'in all fields'
-			},
-			ECOMMERCE: 'eCommerce',
-			CUSTOMERS: {
-				CUSTOMERS: 'Customers',
-				CUSTOMERS_LIST: 'Customers list',
-				NEW_CUSTOMER: 'New Customer',
-				DELETE_CUSTOMER_SIMPLE: {
-					TITLE: 'Customer Delete',
-					DESCRIPTION: 'Are you sure to permanently delete this customer?',
-					WAIT_DESCRIPTION: 'Customer is deleting...',
-					MESSAGE: 'Customer has been deleted'
-				},
-				DELETE_CUSTOMER_MULTY: {
-					TITLE: 'Customers Delete',
-					DESCRIPTION: 'Are you sure to permanently delete selected customers?',
-					WAIT_DESCRIPTION: 'Customers are deleting...',
-					MESSAGE: 'Selected customers have been deleted'
-				},
-				UPDATE_STATUS: {
-					TITLE: 'Status has been updated for selected customers',
-					MESSAGE: 'Selected customers status have successfully been updated'
-				},
-				EDIT: {
-					UPDATE_MESSAGE: 'Customer has been updated',
-					ADD_MESSAGE: 'Customer has been created'
-				}
+				INVALID_EMAIL: 'Invalid E-Mail Adresse',
+				INVALID_DUPLICATE_EMAIL: 'This E-Mail is already used'
 			}
 		}
 	}
